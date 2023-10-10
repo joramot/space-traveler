bba5c077cbb6027adbaee3d316f6f3cd81de5472
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-10-10 11:23:07 -0600

    Create assets folder and add the logoSpace

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import logoSpace from './assets/planet.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logoSpace} alt="logoSpace" />
+      <h1>Space Traveler Hub</h1>
     </div>
   );
 }
