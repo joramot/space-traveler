a549c59d288f5742fc687a89fe5afe3998da5854
alex1779 <alex1779@gmail.com>
2023-10-10 18:48:44 -0300

    Create a route and a view component. Use  for the page navigation links and style active class to indicate which section/page the user is currently on (underline active navigation link).

--- src/components/Errorpage.js
+++ src/components/Errorpage.js
@@ -1,13 +1,6 @@
-import styles from '../styles/Error.module.css';
-
 function ErrorPage() {
   return (
-    <div className={styles.error}>
-      <h2 className={styles.h2}>Ups...Something went wrong!</h2>
-      <p className={styles.p}>
-        404 not found
-      </p>
-    </div>
+    <div />
   );
 }
 
---
+++ src/components/MyProfile.js
@@ -0,0 +1,7 @@
+function MyProfile() {
+  return (
+    <div />
+  );
+}
+
+export default MyProfile;
