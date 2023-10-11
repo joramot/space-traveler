cba503c61e74d2af683c05fea9d2c004bebc2fc9
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-10-11 16:05:44 -0600

    import useSelector to get the state of rockets, add html code  with the information in the return to render lists of rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,8 +1,9 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 function Rockets() {
+  const { rockets, isLoading, error } = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -11,7 +12,15 @@ function Rockets() {
 
   return (
     <main>
-      <p>Rockets</p>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {rockets.map((rocket) => (
+        <article key={rocket.id}>
+          <h1>{rocket.name}</h1>
+          <img src={rocket.flickr_images[0]} alt={rocket.name} />
+          <p>{rocket.description}</p>
+        </article>
+      ))}
     </main>
   );
 }
