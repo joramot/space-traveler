62fa3b7cf51dad3a5ca300f2fc69af43edd0a3bf
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-10-11 14:28:32 -0600

    Add Fetch data from the Rockets endpoint  when the application starts (as Rockets is the default, dispatch an action to store the selected data in Redux store:

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,8 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 function Rockets() {
-  const { rockets, isLoading, error } = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -12,15 +11,7 @@ function Rockets() {
 
   return (
     <main>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {rockets.map((rocket) => (
-        <article key={rocket.id}>
-          <h1>{rocket.name}</h1>
-          <img src={rocket.flickr_images[0]} alt={rocket.name} />
-          <p>{rocket.description}</p>
-        </article>
-      ))}
+      <p>Rockets</p>
     </main>
   );
 }
