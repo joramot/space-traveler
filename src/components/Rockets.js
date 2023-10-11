8d73fafe31364e8bfee432a39258a79a4cc5f87a
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-10-10 23:25:52 -0600

    add Provider to the index.js, fetched data from spacex API, implement dispatch an action to store.js

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
+
 function Rockets() {
+  const { rockets, isLoading, error } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
   return (
-    <div />
+    <main>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {rockets.map((rocket) => (
+        <article key={rocket.id}>
+          <h1>{rocket.name}</h1>
+          <img src={rocket.flickr_images[0]} alt={rocket.name} />
+          <p>{rocket.description}</p>
+        </article>
+      ))}
+    </main>
   );
 }
 
