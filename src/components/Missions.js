899bb5e47cd40ecfcc789a85628a3e8368b5e9cf
alex1779 <alex1779@gmail.com>
2023-10-12 20:05:49 -0300

    Apply changes required

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -4,35 +4,58 @@ import { fetchMissions } from '../redux/missions/missionsSlice';
 import styles from '../styles/Missions.module.css';
 
 function Missions() {
-  const { missions, isLoading, error } = useSelector((state) => state.missions);
+  const {
+    missions, isLoading, error, isLoaded,
+  } = useSelector((state) => state.missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchMissions());
-  }, [dispatch]);
+    if (!isLoaded) {
+      dispatch(fetchMissions());
+    }
+  }, [dispatch, isLoaded]);
 
   return (
     <table>
-      <tr>
-        <h1 className={styles.missions}>Mission</h1>
-        <h1 className={styles.description}>Description</h1>
-        <h1 className={styles.status}>Status</h1>
-        <h1 className={styles.status}> </h1>
-      </tr>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {missions.map((mission) => (
-        <tr key={mission.mission_id}>
-          <h1 className={styles.missions}>{mission.mission_name}</h1>
-          <p className={styles.description_p}>{mission.description}</p>
-          <div className={styles.members}>
-            <h2 className={styles.labelmembers}>NOT A MEMBER</h2>
-          </div>
-          <div className={styles.members}>
-            <button className={styles.joinmission} type="button">Join Mission</button>
-          </div>
+      <tbody>
+        <tr>
+          <td>
+            <h1 className={styles.missions}>Mission</h1>
+          </td>
+          <td>
+            <h1 className={styles.description}>Description</h1>
+          </td>
+          <td>
+            <h1 className={styles.status}>Status</h1>
+          </td>
+          <td>
+            <h1 className={styles.status}> </h1>
+          </td>
+
         </tr>
-      ))}
+        {isLoading && <tr><td><p>Loading...</p></td></tr>}
+        {error && <p>{error}</p>}
+        {missions.map((mission) => (
+          <tr key={mission.mission_id}>
+            <td>
+              <h1 className={styles.missions}>{mission.mission_name}</h1>
+            </td>
+            <td>
+              <p className={styles.description_p}>{mission.description}</p>
+            </td>
+            <td>
+              <div className={styles.members}>
+                <h2 className={styles.labelmembers}>NOT A MEMBER</h2>
+              </div>
+            </td>
+            <td>
+              <div className={styles.members}>
+                <button className={styles.joinmission} type="button">Join Mission</button>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
